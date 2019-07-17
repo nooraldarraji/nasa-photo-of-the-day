8a0cbb8a99c23938b2d7302ec36e0493eda5f81e
Noor Al Darraji <077068@yahoo.com>
2019-07-17 18:57:35 -0500

    Fix errors on nasa.js

--- src/components/nasa.js
+++ src/components/nasa.js
@@ -6,12 +6,13 @@ import axios from 'axios';
 // For additional support, please contact us. When contacting us, please tell us what API you're accessing and provide the following account details so we can quickly find you:
 
 function Nasa() {
-    const [info, setInfo] = useState('')
+    const [info, setInfo] = useState({})
 
     useEffect(() => {
         axios.get('https://api.nasa.gov/planetary/apod?api_key=A4VLWyldR2X1z5WyknQp2Zq54D4nJ1Dc5NOb6maH')
             .then(data => {
                 const getAnswer = data.data;
+                setInfo(getAnswer)
                 console.log(getAnswer);
             })
             .catch(error => { console.log('ERROR OCCURRED: ', error); })
@@ -19,7 +20,8 @@ function Nasa() {
 
     return (
         <div>
-            {info.map(i => <NasaInfo date={i.date} explanation={i.explanation} url={i.url} title={i.title} media_type={i.media_type} />)}
+            <NasaInfo date={info.date} explanation={info.explanation} url={info.url} title={info.title} media_type={info.media_type} />
+            {/* {info.map(i => <NasaInfo date={i.date} explanation={i.explanation} url={i.url} title={i.title} media_type={i.media_type} />)} */}
         </div>
     )
 }
