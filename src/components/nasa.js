5dcc3febb27948122d3e0c1258d9f03859b79bd5
Noor Al Darraji <077068@yahoo.com>
2019-07-18 15:59:43 -0500

    style header and sitewrapper

--- src/components/nasa.js
+++ src/components/nasa.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { NasaInfo } from './nasaInfo';
 import axios from 'axios';
+// import styled from 'styled-components';
 
 // https://api.nasa.gov/planetary/apod?api_key=A4VLWyldR2X1z5WyknQp2Zq54D4nJ1Dc5NOb6maH
 // For additional support, please contact us. When contacting us, please tell us what API you're accessing and provide the following account details so we can quickly find you:
+
+
 
 function Nasa() {
     const [info, setInfo] = useState({})
--- src/components/nasaInfo.js
+++ src/components/nasaInfo.js
@@ -1,17 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    display: flex;
+    border-color: 1px solid red;
+    flex-direction: column;
+    -webkit-box-pack: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    align-items: center;
+    text-align: center;
+    color: white;
+    box-shadow: rgba(0, 0, 0, 0.17) 0px 2px 20px;
+    box-sizing: border-box;
+    min-height: 100vh;
+    background: linear-gradient(20deg, rgb(219, 112, 147), rgb(218, 163, 87));
+`;
+
+const Header = styled.h1`
+    margin: 1em 0 0.5em 0;
+    color: #343434;
+    font-weight: normal;
+    font-family: 'Ultra', sans-serif;   
+    font-size: 36px;
+    line-height: 42px;
+    text-transform: uppercase;
+    text-shadow: 0 2px white, 0 3px #777;
+`;
+
 
 export function NasaInfo({ date, explanation, title, url, media_type }) {
 
     return (
 
-        <div>
-            <h1>{title}</h1>
+        <Wrapper>
+            <Header>{title}</Header>
             <p>{date}</p>
             <p>{explanation}</p>
             <video width="400" controls>
                 <source src={url} type={media_type} />
             </video>
-        </div>
+        </Wrapper>
 
     )
 
